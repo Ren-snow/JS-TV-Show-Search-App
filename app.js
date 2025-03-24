e84a002984addd76c2939a8c490a4c394872d827
Ren <b.b.r.12.11@gmail.com>
2025-03-25 01:33:22 +0900

    Added a function to create elements for each fetched image.

---
+++ app.js
@@ -0,0 +1,38 @@
+const form = document.querySelector("#searchForm");
+const moviesWrapper = document.querySelector("#movies-wrapper");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    if (document.querySelector(".movie-wrapper")) {
+        function removeMovieWrapper(className) {
+            const elements = document.querySelectorAll(`.${className}`);
+            elements.forEach(element => element.remove());
+        }
+        removeMovieWrapper("movie-wrapper");
+    }
+    const searchTerm = form.elements.query.value;
+    const config = { params: { q: searchTerm } };
+    const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+    createMovieWrapper(res.data);
+    form.elements.query.value = "";
+});
+
+const createMovieWrapper = (results) => {
+    let i = 1;
+    for (let result of results) {
+        const movieWrapper = document.createElement("div");
+        movieWrapper.classList.add("movie-wrapper", `movie-wrapper${i}`);
+        const movieName = document.createElement("div");
+        movieName.classList.add("movie-name", `movie-name${i}`);
+        const movieImage = document.createElement("div");
+        movieImage.classList.add("movie-image", `movie-image${i}`);
+        movieWrapper.append(movieName, movieImage);
+
+        const img = document.createElement("IMG");
+        img.src = result.show.image.medium;
+        movieImage.append(img);
+        moviesWrapper.append(movieWrapper);
+        i += 1;
+    }
+};
+
